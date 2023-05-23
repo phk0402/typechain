79363dc595cdd89de65cdadc1b30c5a68c32a7fd
phk0402q <hanafil9742@gmail.com>
2023-05-23 22:17:07 +0900

    Chain

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ interface BlockShape {
     height: number;
     data: string;
 }
+
 class Block implements BlockShape{ // 블록 생성
     public hash: string;
     constructor( // 블록의 데이터 받기
@@ -20,3 +21,30 @@ class Block implements BlockShape{ // 블록 생성
         return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
+
+class Blockchain {
+    private blocks: Block[];
+    constructor() {
+        this.blocks = [];
+    }
+    private getPrevHash(){
+        if (this.blocks.length === 0) return ""
+        return this.blocks[this.blocks.length - 1].hash;
+    }
+    public addBlock(data: string){
+        const newBlock = new Block(this.getPrevHash(), this.blocks.length + 1, data);
+        this.blocks.push(newBlock);
+    }
+    public getBlocks() {
+        return [...this.blocks];
+    }
+}
+
+const blockchain = new Blockchain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+blockchain.addBlock("Fourth one");
+
+console.log(blockchain.getBlocks())
