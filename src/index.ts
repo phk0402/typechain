d332da6ceb62fa0e99aaa27b6597d1bda60ff998
phk0402q <hanafil9742@gmail.com>
2023-05-23 22:00:00 +0900

    DefinitelyTyped

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,17 @@ interface BlockShape {
     height: number;
     data: string;
 }
-class Block implements BlockShape{
+class Block implements BlockShape{ // 블록 생성
     public hash: string;
-    constructor(
+    constructor( // 블록의 데이터 받기
         public prevHash: string,
         public height: number,
         public data: string
     ) {
-        this.hash = Block.calculateHash(prevHash, height, data);
+        this.hash = Block.calculateHash(prevHash, height, data); // 데이터의 해쉬값 생성
     }
     static calculateHash(prevHash: string, height: number, data: string){
         const toHash = `${prevHash}${height}${data}`;
+        return crypto.createHash("sha256").update(toHash).digest("hex");
     }
 }
